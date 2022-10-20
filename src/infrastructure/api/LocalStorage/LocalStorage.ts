99d37dac7ddb2a195165d7dc89bb131b2f644743
Ed <edmundo.presas@kairosds.com>
2022-10-20 17:31:09 +0200

    refactor: Remove unused sessionStorage handler

--- src/infrastructure/api/LocalStorage/LocalStorage.ts
+++ src/infrastructure/api/LocalStorage/LocalStorage.ts
@@ -4,7 +4,7 @@ export const LocalStorage = {
   set: (key: string, value: string): Promise<void> =>
     Promise.resolve(localStorage.setItem(key, value)),
   hasKey: (key: string): boolean =>
-    Object.prototype.hasOwnProperty.call(sessionStorage, key),
+    Object.prototype.hasOwnProperty.call(localStorage, key),
   remove: (key: string): Promise<void> =>
     Promise.resolve(localStorage.removeItem(key)),
 };
